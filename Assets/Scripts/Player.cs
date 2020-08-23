1ce6667728b83f11589f8e6c23497e1a2b431782
ThreeHDM <juanbilardi@gmail.com>
2020-08-23 17:43:15 -0300

    89. ViewPortToWorldPoint() - se limita el movimiento del player usando el metodo ViewPortToWorldPoint() y Mathf.Clamp() para setear los límites de mínimo y máximo.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,37 @@ public class Player : MonoBehaviour
 {
     //Creamos un campo para configurar la velocidad de movimiento
     [SerializeField] float moveSpeed = 10f;
+    //Creamos un campo para configurar el padding de los límites al movimiento
+    [SerializeField] float padding = 1f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //creamos los limites para que el usuario no pueda moverse fuera de la cámara
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        // Almacenamos la cámara principal en gameCamera. Es de tipo Camara
+        Camera gameCamera = Camera.main;
+
+        //ViewportToWorldPoint -> convierte la posición de algo en su relación a lo que ve la cámara a un valor de espacio
+        //fijamos un minimo y un máximo de x (de 0 a 1)
+        //el método toma un vector3(x,y,z) - acá solo usamos x por eso dejamos en 0 los demás.
+        //se almacena cuál es el "world space value" de x en relación con el ViewportValue de x en la cámara
+        //pasamos el value del padding en minimo se lo sumamos (porque es negativo) en máximo se lo restamos)
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        //Eje Y
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -30,11 +56,12 @@ public class Player : MonoBehaviour
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
 
         //la nueva posición en el eje x será nuestra posición actual más una posición delta
-        var newXPos = transform.position.x + deltaX;
+        //Mathf.Clamp - > Clamps the given value between the given minimum float and maximum float values. Returns the given value if it is within the min and max range.
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
 
         //Para que se mueva verticalmente
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         //Le pasamos como primer parametro X la nueva posición calculada del cambio almacenado en deltaX y hacemos igual con deltaY
         transform.position = new Vector2(newXPos, newYPos);
