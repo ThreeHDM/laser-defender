1f691ec657ea89cbd405961da0c33da226c5b9ac
ThreeHDM <juanbilardi@gmail.com>
2020-08-30 12:34:05 -0300

    97. WaveConfig Scriptable Object

---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+
+//Cambiamos de MonoBehaviour a ScriptableObject. A SO is a class that let us store data in stand alone assets. Keeps data out of our scripts. It's lightweight. They don't have to be attached to GameObjects
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f; // creamos este field para agregar variación al spawn
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    //Creamos los getters
+    public GameObject GetEnemyPrefab() { return enemyPrefab;}
+    public GameObject GetPathPrefab() { return pathPrefab; }
+    public float GetTimeBetweenSpawns() { return timeBetweenSpawns; }
+    public float GetSpawnRandomFactor() { return spawnRandomFactor; }
+    public int GetNumberOfEnemies() { return numberOfEnemies; }
+    public float GetMoveSpeed() { return moveSpeed; }
+}
