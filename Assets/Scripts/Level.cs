456ec4995119dab7406bc6e90e482481fca841c9
ThreeHDM <juanbilardi@gmail.com>
2020-11-10 21:57:15 -0300

    112. Load All The Scenes
    -Start and Game over scenes added
    -Fonts added

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+
+    public void LoadGame()
+    {
+        // Se puede cargar una escena con strings o con el scene index. El problema con los strings es que no son escalables. Si cambia el nombre de la escena no cambia
+        SceneManager.LoadScene("Game");
+        Debug.Log("test");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
