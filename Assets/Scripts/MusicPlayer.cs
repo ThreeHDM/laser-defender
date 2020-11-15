f31bbb028f4d763b20992d68135a25730815e255
ThreeHDM <juanbilardi@gmail.com>
2020-11-15 11:16:19 -0300

    114. Music Player With Singleton

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Awake es lo primero que sucede en el juego
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        //GetType() obtiene el tipo de la clase en este caso MusicPlayer
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            //Destruimos el objeto en el que estamos
+            Destroy(gameObject);
+        }
+        else 
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
