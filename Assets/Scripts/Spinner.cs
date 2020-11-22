108d654e37138a023adaa1eba817658cc52e8910
ThreeHDM <juanbilardi@gmail.com>
2020-11-22 07:23:26 -0300

    118. Sorting Layer & Spinning Projectile

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+
+    [SerializeField] float speedOfSpin  = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotamos el indice z (los dos primeros son x,y). A z le pasamos la velocidad x el Tiempo Delta para que sea frame rate independent (This means, no matter how fast or slow a machine is, DeltaTime is smaller or larger accordingly, basically adjusting calculations always to / per second)
+        transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+    }
+}
